a3d35a17f1e26dbefadc6855c62aff80c89e9ecd
Khaled Nowara <Kn_nowara@outlook.com>
2022-01-07 16:28:51 +0100

    Initializing the project

---
+++ Bücheri/app.js
@@ -0,0 +1,21 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+
+
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/',function(req,res){
+  res.render('test',{title:"express"})
+});
+app.listen(3000);
